a6a6acb9b3e423155c25f0073744cbae8f288814
STYJ <simon.tan.yu.jing@gmail.com>
2021-12-19 08:35:36 +0800

    completed flappy dragon

--- flappy/src/main.rs
+++ flappy/src/main.rs
@@ -4,6 +4,8 @@ struct State {
     player: Player,
     frame_time: f32, // you need this to track time between frames to control game speed
     mode: GameMode,
+    obstacle: Obstacle,
+    score: i32,
 }
 
 enum GameMode {
@@ -22,23 +24,38 @@ impl State {
             player: Player::new(5, 25),
             frame_time: 0.0,
             mode: GameMode::Menu,
+            obstacle: Obstacle::new(SCREEN_WIDTH, 0),
+            score: 0,
         }
     }
 
     fn play(&mut self, ctx: &mut BTerm) {
         ctx.cls_bg(NAVY); // similar to cls
         self.frame_time += ctx.frame_time_ms; // the tick function runs as fast as it can so you need to slow the game down. ctx.frame_time_ms = time since last tick() was called.
-        if self.frame_time > FRAME_DURATION { // if it exceeds frame duration, run physics simulation and reset frame_time to 0
+        if self.frame_time > FRAME_DURATION {
+            // if it exceeds frame duration, run physics simulation and reset frame_time to 0
             self.frame_time = 0.0;
             self.player.gravity_and_move();
         }
 
-        if let Some(VirtualKeyCode::Space) = ctx.key { // this is not restricted by frame_time.
+        if let Some(VirtualKeyCode::Space) = ctx.key {
+            // this is not restricted by frame_time.
             self.player.flap();
         }
         self.player.render(ctx);
-        ctx.print(0,0, "Press SPACE to flap.");
-        if self.player.y > SCREEN_HEIGHT { // this is when player has fallen off the screen. remember top left is 0,0
+        ctx.print(0, 0, "Press SPACE to flap.");
+        ctx.print(0, 1, &format!("Score: {}", self.score));
+
+        self.obstacle.render(ctx, self.player.x); // obstacle is encapsulated within the game state
+        if self.player.x > self.obstacle.x {
+            // whenever player moves past obstacle
+            self.score += 1; // add 1 score
+            self.obstacle = Obstacle::new(self.player.x + SCREEN_WIDTH, self.score);
+            // create new obstacle, the reason why you pass in the score is to make the gap smaller
+        }
+
+        if self.player.y > SCREEN_HEIGHT || self.obstacle.hit_obstacle(&self.player) {
+            // this is when player has fallen off the screen. remember top left is 0,0
             self.mode = GameMode::End;
         }
     }
@@ -47,6 +64,8 @@ impl State {
         self.player = Player::new(5, 25);
         self.frame_time = 0.0;
         self.mode = GameMode::Playing;
+        self.score = 0;
+        self.obstacle = Obstacle::new(SCREEN_WIDTH, 0)
     }
 
     fn main_menu(&mut self, ctx: &mut BTerm) {
@@ -70,6 +89,7 @@ impl State {
     fn dead(&mut self, ctx: &mut BTerm) {
         ctx.cls();
         ctx.print_centered(5, "You are dead!!");
+        ctx.print_centered(6, &format!("You earned {} points", self.score));
         ctx.print_centered(8, "(P) Play again");
         ctx.print_centered(9, "(Q) Quit Game!");
 
@@ -124,13 +144,53 @@ impl Player {
         self.y += self.velocity as i32; // cast float to i32 (round down) and add to y coordinate
         self.x += 1; // keep incrementing x as you are moving right
 
-        if self.y < 0 { // ensure that character doesn't fly off screen lol.
+        if self.y < 0 {
+            // ensure that character doesn't fly off screen lol.
             self.y = 0;
         }
     }
 
     fn flap(&mut self) {
         self.velocity = -2.0; // everytime you flap, you set velocity to -2.0 (cancels momentum)
+    }
+}
+
+struct Obstacle {
+    x: i32,     // position in world
+    gap_y: i32, // center of the gap where dragon may pass
+    size: i32,  // length of gap in obstacle
+}
+
+impl Obstacle {
+    fn new(x: i32, score: i32) -> Self {
+        let mut random = RandomNumberGenerator::new();
+        Obstacle {
+            x,
+            gap_y: random.range(10, 40), // randon number between 10<=y<40
+            size: i32::max(2, 20 - score), // the max value between 2 or 20-score.
+        }
+    }
+
+    fn render(&mut self, ctx: &mut BTerm, player_x: i32) {
+        let screen_x = self.x - player_x; // going from world space to player's screen space
+        let half_size = self.size / 2;
+
+        // draw top half of obstacle
+        for y in 0..self.gap_y - half_size {
+            ctx.set(screen_x, y, RED, BLACK, to_cp437('/'));
+        }
+
+        // draw the bottom half of the obstacle
+        for y in self.gap_y + half_size..SCREEN_HEIGHT {
+            ctx.set(screen_x, y, RED, BLACK, to_cp437('/'));
+        }
+    }
+    fn hit_obstacle(&self, player: &Player) -> bool {
+        let half_size = self.size / 2;
+        let does_x_match = player.x == self.x; // if x coordinates match, there MAY be a collision
+        let player_above_gap = player.y < self.gap_y - half_size; // walls are broken down into 2 half (top and bottom)
+        let player_below_gap = player.y > self.gap_y + half_size; // check if the player's y position is in the range of each wall
+        does_x_match && (player_above_gap || player_below_gap)
     }
 }
 
