4a84aa40855aa4b86d8d5993102d3fcef45c5aeb
Simon <simon.tan.yu.jing@gmail.com>
2021-12-25 12:05:05 +0800

    added functions to render map

--- rusty_roguelike/src/map.rs
+++ rusty_roguelike/src/map.rs
@@ -2,3 +2,45 @@ use crate::prelude::*;
 // usize is either u32 or u64 depending on your machine.
 // usize is used to index collections and arrays
 const NUM_TILES: usize = (SCREEN_HEIGHT * SCREEN_WIDTH) as usize;
+
+// Clone allows you to do myTile.clone() which does a deep clone.
+// Copy allows you to copy a type instead of moving the ownership around
+// PartialEq lets you do ==
+#[derive(Clone, Copy, PartialEq)]
+pub enum TileType {
+    // this enum is available to anyone that does "use crate::prelude::*" since prelude is created by importing all of map
+    Wall,
+    Floor,
+}
+
+pub struct Map {
+    pub tiles: Vec<TileType>,
+}
+
+impl Map {
+    pub fn new() -> Self {
+        Self {
+            tiles: vec![TileType::Floor; NUM_TILES], // initialise an array with NUM_TILES number of floor tiles.
+        }
+    }
+    pub fn render(&self, ctx: &mut BTerm) {
+        for y in 0..SCREEN_HEIGHT {
+            for x in 0..SCREEN_WIDTH {
+                let idx = map_idx(x, y);
+                match self.tiles[idx] {
+                    TileType::Floor => {
+                        ctx.set(x, y, YELLOW, BLACK, to_cp437('.'));
+                    }
+                    TileType::Wall => {
+                        ctx.set(x, y, GREEN, BLACK, to_cp437('#'));
+                    }
+                }
+            }
+        }
+    }
+}
+
+// Transforming (x,y) into a single index is known as striding
+pub fn map_idx(x: i32, y: i32) -> usize {
+    ((y * SCREEN_WIDTH) + x) as usize
+}
